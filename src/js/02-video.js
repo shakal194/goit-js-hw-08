97d48e8f5eba26a380b29d68d21b19f946d518d4
Oleksandr Soloviov <shakal194@gmail.com>
2022-02-16 23:07:23 +0200

    update task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo.Player('vimeo-player');
+
+// const onPlay = event => {
+//   console.log(event.seconds);
+//   const storage = localStorage.setItem('videoplayer - current - time', event.seconds);
+// };
+
+// player.on('timeupdate', onPlay, 1000);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    console.log(event.seconds);
+    localStorage.setItem('videoplayer - current - time', event.seconds);
+  }, 1000),
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer - current - time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
