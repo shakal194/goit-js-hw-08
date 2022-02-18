e1d2067526313b7c7e63437fe5aabd00a5db02b5
Oleksandr Soloviov <shakal194@gmail.com>
2022-02-18 22:03:02 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,11 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Vimeo.Player('vimeo-player');
+const STORAGE_SAVE = 'videoplayer - current - time';
 
 // const onPlay = event => {
 //   console.log(event.seconds);
-//   const storage = localStorage.setItem('videoplayer - current - time', event.seconds);
+//   const storage = localStorage.setItem(STORAGE_SAVE, event.seconds);
 // };
 
 // player.on('timeupdate', onPlay, 1000);
@@ -14,12 +15,12 @@ player.on(
   'timeupdate',
   throttle(event => {
     console.log(event.seconds);
-    localStorage.setItem('videoplayer - current - time', event.seconds);
+    localStorage.setItem(STORAGE_SAVE, event.seconds);
   }, 1000),
 );
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer - current - time'))
+  .setCurrentTime(localStorage.getItem(STORAGE_SAVE))
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
+const STORAGE_SAVE = 'feedback-form-state';
 
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+onFormSaved();
+
+function onFormSaved() {
+  const saveStorageMessage = localStorage.getItem(STORAGE_SAVE);
+
+  if (saveStorageMessage) {
+    const { email, message } = JSON.parse(saveStorageMessage);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
+    console.log(saveStorageMessage);
+    form.textarea = saveStorageMessage;
+  }
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(STORAGE_SAVE, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (localStorage.getItem(STORAGE_SAVE) == null) {
+    alert('Заполните поля');
+    console.log('Заполните поля');
+  }
+  console.log(localStorage.getItem(STORAGE_SAVE));
+  localStorage.removeItem(STORAGE_SAVE);
+  event.currentTarget.reset();
+}
